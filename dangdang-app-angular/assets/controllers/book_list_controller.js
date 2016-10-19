6dbb9b85b96086035df66f84714501b9fba6c413
littlesike <mac@zaibuzai.local>
2016-10-19 20:27:08 +0800

    canshu

--- dangdang-app-angular/assets/controllers/book_list_controller.js
+++ dangdang-app-angular/assets/controllers/book_list_controller.js
@@ -1,7 +1,7 @@
 app.controller('bookListController',['$scope','commonService','$routeParams',function($scope,c_s,$routeParams){
      $scope.data=[];
      console.log($routeParams);
-      c_s.getData($routeParams,function(res){
+      c_s.getData('$routeParams',function(res){
           console.dir(res);
       })
 }])
