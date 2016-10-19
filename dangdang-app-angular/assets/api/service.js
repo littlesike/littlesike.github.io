fdb18908575179f8bfe4feb1498459bb84ebf256
littlesike <mac@zaibuzai.local>
2016-10-19 15:39:48 +0800

    创建一个当当网数据Demo

---
+++ dangdang-app-angular/assets/api/service.js
@@ -0,0 +1,15 @@
+app.factory('commonService',['$http',function($http){
+    var service={};
+    service.getData= function(type,callBack){
+        $http({
+            url:'/dangdang-app-angular/data/book_'+type+'.json',
+            method:'get'
+        }).then(function(res){
+            console.log('获取数据成功')
+            callBack(res)
+        },function(err){
+            console.dir(err);
+        })
+    }
+    return service;
+}])
